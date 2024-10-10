c91503c60fde5eec814b71bf6dcb1857895bea9e
Arham Hussain 🥈 <153752282+ARHAM5432@users.noreply.github.com>
2024-10-10 17:47:27 +0500

    Updated with love 🧠

---
+++ app.js
@@ -0,0 +1,26 @@
+//Q1 💫
+
+// var toss = Math.floor(Math.random() * 2) + 1;
+// if (toss === 1) {
+//     document.write("It's Heads!");
+// } else {
+//     document.write("It's Tails!");
+// }
+
+//Q2 💫
+
+// var dice = Math.floor(Math.random() * 6) + 1;
+// document.write("You rolled a " + dice);
+
+
+//Q3 💫
+
+// var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$&()";
+// var charlength = 10;
+// var password = "";
+
+// for (var i = 0; i < charlength; i++) {
+//     var randomIndex = Math.floor(Math.random() * characters.length);
+//     password += characters.charAt(randomIndex);
+// }
+// document.write("Generated password is: " + password);
